dc96093c84a59ff1be404df30d0f9045b3be94bb
Connor-Marble <MarbleConnor@gmail.com>
2015-05-26 10:41:38 -0400

    fixed activity_filter layout

--- app/src/main/java/me/connormarble/streamnotifier/Activities/CreateFilter.java
+++ app/src/main/java/me/connormarble/streamnotifier/Activities/CreateFilter.java
@@ -2,15 +2,29 @@ package me.connormarble.streamnotifier.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
 import me.connormarble.streamnotifier.R;
 
 /**
  * Created by connoc on 5/23/2015.
  */
 public class CreateFilter extends Activity {
+
+    Button saveBtn;
+    Button cancelBtn;
+
+    Switch monSwitch, tueSwitch,
+            wedSwitch, thuSwitch,
+            friSwitch, satSwitch, sunSwitch;
+
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        saveBtn = (Button)findViewById(R.id.save_btn);
     }
-    }
+}
